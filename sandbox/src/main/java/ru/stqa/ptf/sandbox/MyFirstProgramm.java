0c58a5e1231c2aec54f7ff476168cae02af648af
alexfirstborn <92031296+alexfirstborn@users.noreply.github.com>
2021-10-08 15:50:30 +0300

    Update MyFirstProgramm.java

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgramm.java
@@ -3,7 +3,12 @@ package ru.stqa.ptf.sandbox;
 public class MyFirstProgramm {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
+
+    double l = 8.0;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 
 }
