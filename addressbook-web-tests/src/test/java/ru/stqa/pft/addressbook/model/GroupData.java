e38d1ddd4cc0adc34680cbfd3df45672371310cb
alexfirstborn <92031296+alexfirstborn@users.noreply.github.com>
2021-10-28 15:34:38 +0300

    Сортировки списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -20,8 +20,9 @@ public class GroupData {
     return id;
   }
 
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -46,11 +47,6 @@ public class GroupData {
     return footer;
   }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
   public void setId(int id) {
     this.id = id;
   }
@@ -60,7 +56,12 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 }
 
