e985600ce0322f5d52ae7aa82ff40fe8ce9c3a97
alexfirstborn <92031296+alexfirstborn@users.noreply.github.com>
2021-10-29 10:53:41 +0300

    Запуск всех тестов, не выходя из браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
@@ -1,19 +1,19 @@
 package ru.stqa.pft.addressbook.test;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected static final ApplicationManager app = new ApplicationManager();
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
   }
